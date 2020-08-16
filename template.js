0c489cb470d20a6038027d1b05b0f79f04b95fd7
ahmed-hasin-neehal <60386953+ahmed-hasin-neehal@users.noreply.github.com>
2020-08-16 14:06:16 +0600

    template

---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = "Lionel";
+const lastName = "Messi";
+//const line = firstName + " " + lastName + " is the greatest";
+const line = `${firstName} ${lastName} is the greatest`;
+console.log(line);
+
+// const multiLine = "He plays in FC Barcelona\n" + "He plays as a RW\n" + "He wears #10";
+const multiLine = `He plays in FC Barcelona
+He plays as a RW
+He wears #10`;
+console.log(multiLine); 
